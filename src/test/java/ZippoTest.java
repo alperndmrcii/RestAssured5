0ed9babe5c109163d2767fba794521ac1f8c4187
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-26 05:56:38 +0300

    extract JSON path

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -5,6 +5,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -174,10 +175,8 @@ public class ZippoTest {
                 .build();
 
     }
-
-
     @Test
-    public void queryParamTest1() {
+    public void requestResponseSpecification() {
         given()
                 .param("page", 1)
                 .spec(requestSpec)
@@ -191,6 +190,45 @@ public class ZippoTest {
                 .spec(responseSpec)
         ;
     }
+    @Test
+    public void extractingJsonPath() {
+       String countryName= given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .extract().path("country")
+        ;
+        System.out.println("countryName = " + countryName);
+        Assert.assertEquals(countryName,"United States");
+    }
+    @Test
+    public void extractingJsonPath1() {
+        String placeName= given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .extract().path("places[0].'place name'")
+                ;
+        System.out.println("placeName = " + placeName);
+        Assert.assertEquals(placeName,"Beverly Hills");
+       }
+    @Test
+    public void extractingJsonPath2() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki limit bilgisini yazdırınız.
+       int limit= given()
+
+                .when()
+                .get("/users")
+                .then()
+               .extract().path("meta.pagination.limit")
+                ;
+        System.out.println("limit = " + limit);
+
+    }
+
 
 
 }
