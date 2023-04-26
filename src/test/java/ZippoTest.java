2f3fdf86f63f144b1bc30330334153084de87452
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-26 06:28:25 +0300

    extract JSON path all

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -3,11 +3,15 @@ import io.restassured.builder.ResponseBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -159,22 +163,22 @@ public class ZippoTest {
     ResponseSpecification responseSpec;
 
     @BeforeClass
-    public void Setup()
-    {
+    public void Setup() {
         baseURI = "https://gorest.co.in/public/v1";
 
-        requestSpec=new RequestSpecBuilder()
+        requestSpec = new RequestSpecBuilder()
                 .log(LogDetail.URI)
                 .setContentType(ContentType.JSON)
                 .build();
 
-        responseSpec=new ResponseSpecBuilder()
+        responseSpec = new ResponseSpecBuilder()
                 .expectContentType(ContentType.JSON)
                 .expectStatusCode(200)
                 .log(LogDetail.BODY)
                 .build();
 
     }
+
     @Test
     public void requestResponseSpecification() {
         given()
@@ -190,45 +194,102 @@ public class ZippoTest {
                 .spec(responseSpec)
         ;
     }
+
     @Test
     public void extractingJsonPath() {
-       String countryName= given()
+        String countryName = given()
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
 
                 .then()
-                .extract().path("country")
-        ;
+                .extract().path("country");
         System.out.println("countryName = " + countryName);
-        Assert.assertEquals(countryName,"United States");
+        Assert.assertEquals(countryName, "United States");
     }
+
     @Test
     public void extractingJsonPath1() {
-        String placeName= given()
+        String placeName = given()
                 .when()
                 .get("http://api.zippopotam.us/us/90210")
 
                 .then()
                 .log().body()
-                .extract().path("places[0].'place name'")
-                ;
+                .extract().path("places[0].'place name'");
         System.out.println("placeName = " + placeName);
-        Assert.assertEquals(placeName,"Beverly Hills");
-       }
+        Assert.assertEquals(placeName, "Beverly Hills");
+    }
+
     @Test
     public void extractingJsonPath2() {
         // https://gorest.co.in/public/v1/users  dönen değerdeki limit bilgisini yazdırınız.
-       int limit= given()
+        int limit = given()
 
                 .when()
                 .get("/users")
                 .then()
-               .extract().path("meta.pagination.limit")
-                ;
+                // .log().body()
+                .extract().path("meta.pagination.limit");
         System.out.println("limit = " + limit);
 
     }
 
+    @Test
+    public void extractingJsonPath3() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki datadaki bütün id bilgilerini yazdırınız.
+
+      List<Integer> idler =
+              given()
+
+                .when()
+                .get("/users")
+
+                .then()
+              .extract().path("data.id")
+        ;
+        System.out.println("idler = " + idler);
+    }
+    @Test
+    public void extractingJsonPath4() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki datadaki bütün name bilgilerini yazdırınız.
+        List<String> names= given()
 
 
+                .when()
+                .get("/users")
+
+                .then()
+                .extract().path("data.name")
+        ;
+        System.out.println("names = " + names);
+
+    }
+    @Test
+    public void extractingJsonPathResponsAll() {
+        // https://gorest.co.in/public/v1/users  dönen değerdeki bütün name lei yazdırınız.
+
+        Response donenData =
+                given()
+
+                        .when()
+                        .get("https://gorest.co.in/public/v1/users")
+
+                        .then()
+                        .statusCode(200)
+                        // .log().body()
+                        .extract().response(); // dönen tüm datayı verir.
+        ;
+
+        List<Integer> idler= donenData.path("data.id");
+        List<String> names= donenData.path("data.name");
+        int limit= donenData.path("meta.pagination.limit");
+
+        System.out.println("idler = " + idler);
+        System.out.println("names = " + names);
+        System.out.println("limit = " + limit);
+
+        Assert.assertTrue(names.contains("Sanjay Mukhopadhyay"));
+        Assert.assertTrue(idler.contains(1203758));
+        Assert.assertEquals(limit, 10, "test sonucu hatalı");
+    }
 }
