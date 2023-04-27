bb438d144c66c75a8543af39cff2a8bcdfdc64dc
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-27 14:56:44 +0300

    Task 4

--- src/test/java/Tasks.java
+++ src/test/java/Tasks.java
@@ -1,6 +1,7 @@
 import Model.Location;
 import Model.ToDo;
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -41,7 +42,7 @@ public class Tasks {
     @Test
     public void Task1() {
 
-       ToDo todo= given()
+        ToDo todo = given()
 
                 .when()
                 .get("https://jsonplaceholder.typicode.com/todos/2")
@@ -50,9 +51,56 @@ public class Tasks {
                 .then()
                 .log().body()
                 .statusCode(200)
-               .extract().body().as(ToDo.class)
-        ;
+                .extract().body().as(ToDo.class);
         System.out.println("todo = " + todo);
         System.out.println("todo.getTitle() = " + todo.getTitle());
     }
+
+    /**
+     * Task 3
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     * expect title in response body to be "quis ut nam facilis et officia qui"
+     */
+    @Test
+    public void Task3() {
+      given()
+
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title",equalTo("quis ut nam facilis et officia qui"))
+                ;
+
+    }
+    /**
+     Task 4
+     create a request to https://jsonplaceholder.typicode.com/todos/2
+     expect status 200
+     expect content type JSON
+     expect response completed status to be false
+     extract completed field and testNG assertion
+     */
+    @Test
+    public void Task4(){
+        ToDo todo=given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+                .then()
+                .statusCode(200)
+                .body("completed",equalTo(false))
+                .contentType(ContentType.JSON)
+                .extract().body().as(ToDo.class)
+        ;
+        Assert.assertTrue(todo.getCompleted().equals(false));
+    }
+
+
 }
