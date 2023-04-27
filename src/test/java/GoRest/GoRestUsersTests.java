94d7e7c90ae01fe9b67d6893e9a83b4dae9c78d9
Alperen Demirci <alperndmrcii@gmail.com>
2023-04-27 16:30:43 +0300

    Create User Json, Create User Map, Create User Class

--- src/test/java/GoRest/GoRestUsersTests.java
+++ src/test/java/GoRest/GoRestUsersTests.java
@@ -1,39 +1,120 @@
 package GoRest;
 
+import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class GoRestUsersTests {
+    Faker randomUretici = new Faker();
+    int userID;
+
     @Test
-    public void CreateUser() {
+    public void CreateUserJson() {
         // https://gorest.co.in/public/v2/users
         //  {"name":"{{$randomFullName}}", "gender":"male", "email":"{{$randomEmail}}", "status":"active"}
         // a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801
-        int userID=
-        given()
-                .header("Authorization","Bearer a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801")
-                .contentType(ContentType.JSON) // gönderilecek data JSON
-                .body("{\"name\":\"ismet temur3535\", \"gender\":\"male\", \"email\":\"ismet3535@gmail.com\", \"status\":\"active\"}")
-                .log().uri()
-                .log().body()
-                .when()
-                .post("https://gorest.co.in/public/v2/users")
 
-                .then()
-                .log().body()
-                .statusCode(201)
-                .contentType(ContentType.JSON)
-                .extract().path("id")
-                ;
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+
+        userID =
+                given()
+                        .header("Authorization", "Bearer a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801")
+                        .contentType(ContentType.JSON) // gönderilecek data JSON
+                        .body("{\"name\":\"" + rndFullname + "\", \"gender\":\"male\", \"email\":\"" + rndEmail + "\", \"status\":\"active\"}")
+                        //.log().uri()
+                        //.log().body()
+                        .when()
+                        .post("https://gorest.co.in/public/v2/users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id")
+        ;
 
     }
 
-    @Test
+    @Test(dependsOnMethods = "CreateUser")
     public void GetUserByID() {
+        given()
+                .header("Authorization", "Bearer a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801")
 
+                .when()
+                .get("https://gorest.co.in/public/v2/users/"+userID)
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id",equalTo(userID))
+
+
+                ;
+
+    }
+    @Test
+    public void CreateUserMap() {
+
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+
+        Map<String,String> newUser=new HashMap<>();
+        newUser.put("name",rndFullname);
+        newUser.put("gender","male");
+        newUser.put("email",rndEmail);
+        newUser.put("status","active");
+        userID =
+                given()
+                        .header("Authorization", "Bearer a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801")
+                        .contentType(ContentType.JSON) // gönderilecek data JSON
+                        .body(newUser)
+                        //.log().uri()
+                        //.log().body()
+                        .when()
+                        .post("https://gorest.co.in/public/v2/users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id")
+        ;
+
+    }
+    @Test
+    public void CreateUserClass() {
+
+        String rndFullname = randomUretici.name().fullName();
+        String rndEmail = randomUretici.internet().emailAddress();
+        User newUser=new User();
+        newUser.name=rndFullname;
+        newUser.gender="male";
+        newUser.email=rndEmail;
+        newUser.status="active";
+        userID =
+                given()
+                        .header("Authorization", "Bearer a3595aa2d2f40a1af2fdc5aff7d7b5e5f6564955bcebc5c21ab3aba805dff801")
+                        .contentType(ContentType.JSON) // gönderilecek data JSON
+                        .body(newUser)
+                        //.log().uri()
+                        //.log().body()
+                        .when()
+                        .post("https://gorest.co.in/public/v2/users")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .contentType(ContentType.JSON)
+                        .extract().path("id")
+        ;
     }
 
     @Test
---
+++ src/test/java/GoRest/User.java
@@ -0,0 +1,12 @@
+package GoRest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User {
+  String name;
+  String gender;
+  String email;
+  String status;
+
+}
